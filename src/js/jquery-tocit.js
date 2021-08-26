c3461e424d3c051fa39ad2e5cf97a4f7dec40d48
Julien Colot <Julien.Colot@eurid.eu>
2021-08-26 18:54:50 +0200

    Bug fix

--- src/js/jquery-tocit.js
+++ src/js/jquery-tocit.js
@@ -206,19 +206,19 @@ window.$ = jQuery;
             let ul;
 
 
-            // If the selectors option has a comma within the string
-            if (!this.options.selectors.indexOf(',') === -1) {
-
-                // Grabs the first selector from the string
-                firstElem = $(this.options.context).find(this.options.selectors.replace(/ /g, '').substr(0, this.options.selectors.indexOf(',')));
-
             // If the selectors option does not have a comma within the string
-            } else {
-
+            if (this.options.selectors.indexOf(',') === -1) {
                 // Grabs the first selector from the string and makes sure there are no spaces
                 firstElem = $(this.options.context).find(this.options.selectors.replace(/ /g, ''));
 
-            }
+
+            // If the selectors option has a comma within the string
+            } else {
+                 // Grabs the first selector from the string
+                firstElem = $(this.options.context).find(this.options.selectors.replace(/ /g, '').substr(0, this.options.selectors.indexOf(',')));
+           
+
+             }
 
             if (!firstElem.length) {
 
